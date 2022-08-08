933a5b6774c1ddf9a080ef643b5cee56e5b42138
alexey.sheblykin <alexey.sheblykin1@gmail.com>
2022-08-08 16:37:57 -0400

    architecture

---
+++ src/main/java/com/alexey/sheblykin/dto/VacancyIndeedInfoDto.java
@@ -0,0 +1,76 @@
+package com.alexey.sheblykin.dto;
+
+import java.util.Objects;
+
+public class VacancyIndeedInfoDto {
+
+    private long id;
+    private String title;
+    private String location;
+    private String salary;
+    private String date;
+
+    public VacancyIndeedInfoDto() {
+    }
+
+    public VacancyIndeedInfoDto(long id, String title, String location, String salary, String date) {
+        this.id = id;
+        this.title = title;
+        this.location = location;
+        this.salary = salary;
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VacancyIndeedInfoDto that = (VacancyIndeedInfoDto) o;
+        return Objects.equals(getTitle(), that.getTitle()) && Objects.equals(getLocation(), that.getLocation()) && Objects.equals(getSalary(), that.getSalary()) && Objects.equals(getDate(), that.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getLocation(), getSalary(), getDate());
+    }
+}
---
+++ src/main/java/com/alexey/sheblykin/service/VacancyDataService.java
@@ -0,0 +1,14 @@
+package com.alexey.sheblykin.service;
+
+import com.alexey.sheblykin.dto.VacancyIndeedInfoDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VacancyDataService {
+
+    public List<VacancyIndeedInfoDto> loadCompanyVacancies(int page, long companyId) {
+        return null;
+    }
+}
